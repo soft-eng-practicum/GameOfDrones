1d4a8f24cd6efe892cc1493b59d600386e7cf765
Steven B <sbeteag@ggc.edu>
2017-09-27 19:55:02 -0400

    Took my name off file. Added keyboard shortcuts.

--- DemoGUI.py
+++ DemoGUI.py
@@ -1,6 +1,7 @@
 from pyardrone import ARDrone
 
 import sys
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtCore import pyqtSlot
 
@@ -16,57 +17,58 @@ class App(QWidget):
         self.width = 420
         self.height = 240
         self.initUI()
+        self.setWindowFlags(Union=None, Qt_WindowFlags=())
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        forwards_btn = QPushButton('^', self)
+        forwards_btn = QPushButton('&w', self)
         forwards_btn.setToolTip('Moves the drone forward')
         forwards_btn.move(100, 80)
         forwards_btn.clicked.connect(self.forward)
 
-        backwards_btn = QPushButton('v', self)
+        backwards_btn = QPushButton('&s', self)
         backwards_btn.setToolTip('Moves the drone backward')
         backwards_btn.move(100, 140)
         backwards_btn.clicked.connect(self.backward)
 
-        right_btn = QPushButton('>', self)
+        right_btn = QPushButton('&d', self)
         right_btn.setToolTip('Moves the drone right')
         right_btn.move(160, 110)
         right_btn.clicked.connect(self.right)
 
-        left_btn = QPushButton('<', self)
+        left_btn = QPushButton('&a', self)
         left_btn.setToolTip('Moves the drone move_left')
         left_btn.move(40, 110)
         left_btn.clicked.connect(self.move_left)
 
-        cw_btn = QPushButton('CW->', self)
+        cw_btn = QPushButton('&e', self)
         cw_btn.setToolTip('Rotates the drone cw')
         cw_btn.move(160, 40)
         cw_btn.clicked.connect(self.cw)
 
-        ccw_btn = QPushButton('<-CCW', self)
+        ccw_btn = QPushButton('&q', self)
         ccw_btn.setToolTip('Rotates the drone ccw')
         ccw_btn.move(40, 40)
         ccw_btn.clicked.connect(self.ccw)
 
-        increase_alt_btn = QPushButton('IncreaseAlt', self)
+        increase_alt_btn = QPushButton('&IncreaseAlt', self)
         increase_alt_btn.setToolTip('Increase Altitude')
         increase_alt_btn.move(280, 50)
         increase_alt_btn.clicked.connect(self.increase_alt)
 
-        decrease_alt_btn = QPushButton('DecreaseAlt', self)
+        decrease_alt_btn = QPushButton('De&creaseAlt', self)
         decrease_alt_btn.setToolTip('Decrease Altitude')
         decrease_alt_btn.move(280, 80)
         decrease_alt_btn.clicked.connect(self.decrease_alt)
 
-        takeoff_btn = QPushButton('Takeoff', self)
+        takeoff_btn = QPushButton('&Takeoff', self)
         takeoff_btn.setToolTip('Takeoff')
         takeoff_btn.move(280, 150)
         takeoff_btn.clicked.connect(self.takeoff)
 
-        land_btn = QPushButton('Land', self)
+        land_btn = QPushButton('&Land', self)
         land_btn.setToolTip('Land')
         land_btn.move(280, 180)
         land_btn.clicked.connect(self.land)
